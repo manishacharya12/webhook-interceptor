ce06fc0d6198b50f903fb9cc8d0454041eb910e9
manishacharya12 <manish.acharya@appdirect.com>
2024-12-02 18:29:15 +0530

    added corsorigin annotation for test purpose and logs

--- src/main/java/com/appdirect/devtools/webhooks/controller/SessionController.java
+++ src/main/java/com/appdirect/devtools/webhooks/controller/SessionController.java
@@ -1,12 +1,13 @@
 package com.appdirect.devtools.webhooks.controller;
 
 import java.util.List;
-import java.util.UUID;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.appdirect.devtools.webhooks.dto.SessionDto;
 import com.appdirect.devtools.webhooks.service.SessionService;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = {"/api/v1/sessions"})
 public class SessionController {
 
@@ -25,21 +28,25 @@ public class SessionController {
 
 	@PostMapping(path = {"/"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SessionDto> generateSession() {
+		log.info("Received session generation request");
 		return ResponseEntity.ok(sessionService.generateSession());
 	}
 
 	@GetMapping(path = {"/"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<SessionDto>> getAllSessions() {
+		log.info("Received request to fetch all sessions");
 		return ResponseEntity.ok(sessionService.getAllSessions());
 	}
 
 	@GetMapping(path = {"/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SessionDto> getSessionById(@PathVariable long id) {
+		log.info("Received request to fetch session by Id={}", id);
 		return ResponseEntity.ok(sessionService.getSessionById(id));
 	}
 
 	@GetMapping(path = {"/uuid/{uuid}"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SessionDto> getSessionByUUID(@PathVariable String uuid) {
+		log.info("Received request to fetch session by UUID={}", uuid);
 		return ResponseEntity.ok(sessionService.getSessionByUUID(uuid));
 	}
 }
--- src/main/java/com/appdirect/devtools/webhooks/controller/WebhookRequestsController.java
+++ src/main/java/com/appdirect/devtools/webhooks/controller/WebhookRequestsController.java
@@ -8,9 +8,11 @@ import java.util.Map;
 import java.util.UUID;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,11 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.appdirect.devtools.webhooks.dto.WebhookRequestDto;
 import com.appdirect.devtools.webhooks.service.SessionService;
 import com.appdirect.devtools.webhooks.service.WebhookRequestService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(value = {"/api/v1/sessions"})
 public class WebhookRequestsController {
 
@@ -35,16 +38,19 @@ public class WebhookRequestsController {
 
 	@GetMapping(path = {"/{sessionUuid}/sessionrequests"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<WebhookRequestDto>> getAllWebhookRequestsBySessionUuid(@PathVariable String sessionUuid) {
+		log.info("Received request with to get webhooks by session uuid={}", sessionUuid);
 		return ResponseEntity.ok(webhookRequestService.getAllWebhookRequestsBySessionUuid(sessionUuid));
 	}
 
 	@GetMapping(path = {"/{sessionUuid}/requests/{webhookId}"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<WebhookRequestDto> getWebhookRequestBySessionUuidAndWebhookId(@PathVariable String sessionUuid, @PathVariable long webhookId) {
+		log.info("Received request with method to get webhook by session uuid={} and webhook Id={}", sessionUuid, webhookId);
 		return ResponseEntity.ok(webhookRequestService.getWebhookRequestBySessionUuidAndWebhookId(sessionUuid, webhookId));
 	}
 
 	@GetMapping(path = {"/{sessionUuid}/requests/uuid/{webhookUuid}"}, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<WebhookRequestDto> getWebhookRequestBySessionUuidAndWebhookUuid(@PathVariable String sessionUuid, @PathVariable String webhookUuid) {
+		log.info("Received request with method to get webhook by session uuid={} and webhook UUUID={}", sessionUuid, webhookUuid);
 		return ResponseEntity.ok(webhookRequestService.getWebhookRequestBySessionUuidAndWebhookUuid(sessionUuid, webhookUuid));
 	}
 
@@ -56,6 +62,7 @@ public class WebhookRequestsController {
 		@RequestParam(required=false) Map<String, String> queryParams,
 		@RequestHeader(required=false) Map<String, String> headers) {
 
+		log.info("Received a webhook request with method={} and host={}", request.getMethod(), request.getRemoteHost());
 		long sessionId = sessionService.getSessionByUUID(sessionUuid).getId();
 		UUID generatedUuid = UUID.randomUUID();
 		ZonedDateTime creationTime = ZonedDateTime.now(ZoneId.of("UTC"));
